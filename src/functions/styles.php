83b133b569fcec219bc92546d1bfa35160dd1eff
P.D. Rittenhouse <pdee999@gmail.com>
2023-05-29 23:38:35 -0400

    Adding block styles

--- src/functions/styles.php
+++ src/functions/styles.php
@@ -93,3 +93,31 @@ function dream_child_acf_block_editor_style()
 if (is_admin()) {
 	add_action('enqueue_block_assets', 'dream_child_acf_block_editor_style');
 }
+
+
+/**
+ * Child Block Styles
+ * @link https://jasonyingling.me/enqueueing-scripts-and-styles-for-gutenberg-blocks/
+ */
+
+function dream_enqueue_child_block_scripts() {
+    $child_theme_blocks_path = dirname(__DIR__, 2) . '/src/templates/blocks';
+    $child_theme_blocks = array_filter(scandir($child_theme_blocks_path), 'filter_block_dir');
+    foreach ($child_theme_blocks as $block) {
+        if ( file_exists( $child_theme_blocks_path . '/' . $block . '/script.js' ) ) {
+            wp_enqueue_script('child_blocks_script_' . $block, get_stylesheet_directory_uri() . '/src/templates/blocks/' . $block . '/script.js', array ( 'jquery', 'acf-input' ), wp_get_theme()->get( 'Version' ), 'all');
+        }
+    }
+}
+add_action( 'enqueue_block_assets', 'dream_enqueue_child_block_scripts' );
+
+function dream_enqueue_child_block_admin_scripts() {
+    $child_theme_blocks_path = dirname(__DIR__, 2) . '/src/templates/blocks';
+    $child_theme_blocks = array_filter(scandir($child_theme_blocks_path), 'filter_block_dir');
+    foreach ($child_theme_blocks as $block) {
+        if ( file_exists( $child_theme_blocks_path . '/' . $block . '/index.js' ) ) {
+            wp_enqueue_script('child_blocks_admin_script_' . $block, get_stylesheet_directory_uri() . '/src/templates/blocks/' . $block . '/index.js', array ( 'jquery', 'acf-input' ), wp_get_theme()->get( 'Version' ), 'all');
+        }
+    }
+}
+add_action( 'enqueue_block_editor_assets', 'dream_enqueue_child_block_admin_scripts' );
