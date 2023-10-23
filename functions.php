d8aa4ffcf61a32a8aa805a1aaedd54fd466e6273
P.D. Rittenhouse <pdee999@gmail.com>
2023-10-22 23:06:12 -0400

    Update functions.php

--- functions.php
+++ functions.php
@@ -6,7 +6,10 @@
 $dream_child_includes = array(
     "scripts.php",
     "styles.php",
-    "blocks.php"
+    "blocks.php",
+    "redirects.php",
+    "rewrites.php",
+    "routes.php"
 );
 
 foreach($dream_child_includes as $inc){
