26e3451c06db9befd74b2312551317df86e7af5b
P.D. Rittenhouse <pdee999@gmail.com>
2023-10-22 23:03:48 -0400

    Create redirects.php

---
+++ src/functions/redirects.php
@@ -0,0 +1,10 @@
+<?php
+
+// @link: https://developer.wordpress.org/reference/functions/wp_safe_redirect/
+// function sample_redirect() {
+//     if (is_page('page-to-redirect')) {
+//         wp_safe_redirect(home_url('/destination-page-slug/'));
+//         exit;
+//     }
+// }
+// add_action('template_redirect', 'sample_redirect');
