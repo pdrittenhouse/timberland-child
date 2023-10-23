36ea34c9405e990087b5440b670d3b716d437f7f
P.D. Rittenhouse <pdee999@gmail.com>
2023-10-22 23:04:41 -0400

    Create rewrites.php

---
+++ src/functions/rewrites.php
@@ -0,0 +1,9 @@
+<?php
+
+// @link: https://developer.wordpress.org/reference/functions/add_rewrite_rule/
+//function sample_rewrite_pagination()
+//{
+//    add_rewrite_rule('^page-slug/page/([0-9]+)/?$', 'index.php?page_id=3560&paged=$matches[1]', 'top');
+//}
+//
+//add_action('init', 'sample_rewrite_pagination');
