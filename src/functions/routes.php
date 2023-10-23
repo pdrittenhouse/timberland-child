7f1c09b788a868787e9707889f65fcd031e760fd
P.D. Rittenhouse <pdee999@gmail.com>
2023-10-22 23:05:07 -0400

    Create routes.php

---
+++ src/functions/routes.php
@@ -0,0 +1,7 @@
+<?php
+
+// @link: https://timber.github.io/docs/guides/routing/
+// Routes::map('blog/:name', function ($params) {
+//     $query = 'posts_per_page=3&post_type=' . $params['name'];
+//     Routes::load('archive.php', null, $query, 200);
+// });
