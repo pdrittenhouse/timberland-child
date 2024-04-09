c122d0a4bf41a131c5c0eb6f2970c3465aa6ae3e
P.D. Rittenhouse <pdee999@gmail.com>
2024-04-08 23:09:58 -0400

    Update scripts.php

--- src/functions/scripts.php
+++ src/functions/scripts.php
@@ -67,27 +67,29 @@ function dream_enqueue_child_block_scripts() {
     $child_theme_blocks_path = dirname(__DIR__, 2) . '/src/templates/blocks';
     $child_theme_blocks = array_filter(scandir($child_theme_blocks_path), 'filter_block_dir');
 
-    // Get the post content
-    $post_content = get_post()->post_content;
+    if (get_post() !== null) {
+        // Get the post content
+        $post_content = get_post()->post_content;
 
-    foreach ($child_theme_blocks as $block) {
-        // Get the block.json file path
-        $block_json_path = $child_theme_blocks_path . '/' . $block . '/block.json';
+        foreach ($child_theme_blocks as $block) {
+            // Get the block.json file path
+            $block_json_path = $child_theme_blocks_path . '/' . $block . '/block.json';
 
-        if (file_exists($block_json_path)) {
-            // Read the block.json file
-            $block_json_content = file_get_contents($block_json_path);
-            $block_data = json_decode($block_json_content, true);
+            if (file_exists($block_json_path)) {
+                // Read the block.json file
+                $block_json_content = file_get_contents($block_json_path);
+                $block_data = json_decode($block_json_content, true);
 
-            // Check if the block name is defined in block.json
-            if (isset($block_data['name'])) {
-                $block_name = $block_data['name'];
+                // Check if the block name is defined in block.json
+                if (isset($block_data['name'])) {
+                    $block_name = $block_data['name'];
 
-                // Check if the post content contains the block
-                if (has_block($block_name, $post_content)) {
-                    // Enqueue the child theme block script
-                    if (file_exists($child_theme_blocks_path . '/' . $block . '/script.js')) {
-                        wp_enqueue_script('child_blocks_script_' . $block, get_stylesheet_directory_uri() . '/src/templates/blocks/' . $block . '/script.js', array('jquery', 'acf-input'), wp_get_theme()->get('Version'), 'all');
+                    // Check if the post content contains the block
+                    if (has_block($block_name, $post_content)) {
+                        // Enqueue the child theme block script
+                        if (file_exists($child_theme_blocks_path . '/' . $block . '/script.js')) {
+                            wp_enqueue_script('child_blocks_script_' . $block, get_stylesheet_directory_uri() . '/src/templates/blocks/' . $block . '/script.js', array('jquery', 'acf-input'), wp_get_theme()->get('Version'), 'all');
+                        }
                     }
                 }
             }
@@ -111,27 +113,29 @@ function dream_enqueue_child_block_admin_scripts() {
     $child_theme_blocks_path = dirname(__DIR__, 2) . '/src/templates/blocks';
     $child_theme_blocks = array_filter(scandir($child_theme_blocks_path), 'filter_block_dir');
 
-    // Get the post content
-    $post_content = get_post()->post_content;
+    if (get_post() !== null) {
+        // Get the post content
+        $post_content = get_post()->post_content;
 
-    foreach ($child_theme_blocks as $block) {
-        // Get the block.json file path
-        $block_json_path = $child_theme_blocks_path . '/' . $block . '/block.json';
+        foreach ($child_theme_blocks as $block) {
+            // Get the block.json file path
+            $block_json_path = $child_theme_blocks_path . '/' . $block . '/block.json';
 
-        if (file_exists($block_json_path)) {
-            // Read the block.json file
-            $block_json_content = file_get_contents($block_json_path);
-            $block_data = json_decode($block_json_content, true);
+            if (file_exists($block_json_path)) {
+                // Read the block.json file
+                $block_json_content = file_get_contents($block_json_path);
+                $block_data = json_decode($block_json_content, true);
 
-            // Check if the block name is defined in block.json
-            if (isset($block_data['name'])) {
-                $block_name = $block_data['name'];
+                // Check if the block name is defined in block.json
+                if (isset($block_data['name'])) {
+                    $block_name = $block_data['name'];
 
-                // Check if the post content contains the block
-                if (has_block($block_name, $post_content)) {
-                    // Enqueue the child theme block admin script
-                    if (file_exists($child_theme_blocks_path . '/' . $block . '/index.js')) {
-                        wp_enqueue_script('child_blocks_admin_script_' . $block, get_stylesheet_directory_uri() . '/src/templates/blocks/' . $block . '/index.js', array('jquery', 'acf-input'), wp_get_theme()->get('Version'), 'all');
+                    // Check if the post content contains the block
+                    if (has_block($block_name, $post_content)) {
+                        // Enqueue the child theme block admin script
+                        if (file_exists($child_theme_blocks_path . '/' . $block . '/index.js')) {
+                            wp_enqueue_script('child_blocks_admin_script_' . $block, get_stylesheet_directory_uri() . '/src/templates/blocks/' . $block . '/index.js', array('jquery', 'acf-input'), wp_get_theme()->get('Version'), 'all');
+                        }
                     }
                 }
             }
