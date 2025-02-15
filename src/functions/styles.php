a8388db190f3a1312047c24603d20328e6aab2ff
Patrick Rittenhouse <pdee999@gmail.com>
2025-02-15 14:04:50 -0500

    adding logo slider block and other child theme updates

--- src/functions/styles.php
+++ src/functions/styles.php
@@ -100,6 +100,61 @@ if (is_admin()) {
  * @link https://jasonyingling.me/enqueueing-scripts-and-styles-for-gutenberg-blocks/
  */
 
+// Function to check and enqueue child block styles
+function check_and_enqueue_child_block_styles($content, $child_theme_blocks_path, $child_theme_blocks) {
+    foreach ($child_theme_blocks as $block) {
+        // Get the block.json file path
+        $block_json_path = $child_theme_blocks_path . '/' . $block . '/block.json';
+
+
+        if (file_exists($block_json_path)) {
+            // Read the block.json file
+            $block_json_content = file_get_contents($block_json_path);
+            $block_data = json_decode($block_json_content, true);
+
+
+            // Check if the block name is defined in block.json
+            if (isset($block_data['name'])) {
+                $block_name = $block_data['name'];
+
+
+                // Check if the content contains the block
+                if (has_block($block_name, $content)) {
+                    // Enqueue the child theme block style
+                    if (file_exists($child_theme_blocks_path . '/' . $block . '/style.css')) {
+                        wp_enqueue_style('child_blocks_style_' . $block, get_stylesheet_directory_uri() . '/src/templates/blocks/' . $block . '/style.css', array(), wp_get_theme()->get('Version'), 'all');
+                    }
+                }
+            }
+        }
+    }
+}
+
+function dream_enqueue_child_block_styles() {
+    $child_theme_blocks_path = dirname(__DIR__, 2) . '/src/templates/blocks';
+    $child_theme_blocks = array_filter(scandir($child_theme_blocks_path), 'filter_block_dir');
+
+
+    if (get_post() !== null) {
+        // Get the post content
+        $post_content = get_post()->post_content;
+
+
+        // Check post content
+        check_and_enqueue_child_block_styles($post_content, $child_theme_blocks_path, $child_theme_blocks);
+
+
+        // Parse patterns and check content within patterns
+        if (preg_match_all('/<!-- wp:block {"ref":(\d+)} \/-->/', $post_content, $matches)) {
+            foreach ($matches[1] as $pattern_id) {
+                $pattern_content = get_post($pattern_id)->post_content;
+                check_and_enqueue_child_block_styles($pattern_content, $child_theme_blocks_path, $child_theme_blocks);
+            }
+        }
+    }
+}
+add_action('enqueue_block_assets', 'dream_enqueue_child_block_styles');
+
 //function dream_enqueue_child_block_styles() {
 //    $child_theme_blocks_path = dirname(__DIR__, 2) . '/src/templates/blocks';
 //    $child_theme_blocks = array_filter(scandir($child_theme_blocks_path), 'filter_block_dir');
@@ -111,38 +166,61 @@ if (is_admin()) {
 //}
 //add_action( 'enqueue_block_assets', 'dream_enqueue_child_block_styles' );
 
-function dream_enqueue_child_block_styles() {
-    $child_theme_blocks_path = dirname(__DIR__, 2) . '/src/templates/blocks';
-    $child_theme_blocks = array_filter(scandir($child_theme_blocks_path), 'filter_block_dir');
 
-    // Get the post content
-    $post_content = get_post()->post_content;
-
+// Function to check and enqueue child block admin styles
+function check_and_enqueue_child_block_admin_styles($content, $child_theme_blocks_path, $child_theme_blocks) {
     foreach ($child_theme_blocks as $block) {
         // Get the block.json file path
         $block_json_path = $child_theme_blocks_path . '/' . $block . '/block.json';
+
 
         if (file_exists($block_json_path)) {
             // Read the block.json file
             $block_json_content = file_get_contents($block_json_path);
             $block_data = json_decode($block_json_content, true);
 
+
             // Check if the block name is defined in block.json
             if (isset($block_data['name'])) {
                 $block_name = $block_data['name'];
 
-                // Check if the post content contains the block
-                if (has_block($block_name, $post_content)) {
-                    // Enqueue the child theme block style
-                    if (file_exists($child_theme_blocks_path . '/' . $block . '/style.css')) {
-                        wp_enqueue_style('child_blocks_style_' . $block, get_stylesheet_directory_uri() . '/src/templates/blocks/' . $block . '/style.css', array(), wp_get_theme()->get('Version'), 'all');
+
+                // Check if the content contains the block
+                if (has_block($block_name, $content)) {
+                    // Enqueue the child theme block admin style
+                    if (file_exists($child_theme_blocks_path . '/' . $block . '/index.css')) {
+                        wp_enqueue_style('child_blocks_admin_style_' . $block, get_stylesheet_directory_uri() . '/src/templates/blocks/' . $block . '/index.css', array(), wp_get_theme()->get('Version'), 'all');
                     }
                 }
             }
         }
     }
 }
-add_action('enqueue_block_assets', 'dream_enqueue_child_block_styles');
+
+function dream_enqueue_child_block_admin_styles() {
+    $child_theme_blocks_path = dirname(__DIR__, 2) . '/src/templates/blocks';
+    $child_theme_blocks = array_filter(scandir($child_theme_blocks_path), 'filter_block_dir');
+
+
+    if (get_post() !== null) {
+        // Get the post content
+        $post_content = get_post()->post_content;
+
+
+        // Check post content
+        check_and_enqueue_child_block_admin_styles($post_content, $child_theme_blocks_path, $child_theme_blocks);
+
+
+        // Parse patterns and check content within patterns
+        if (preg_match_all('/<!-- wp:block {"ref":(\d+)} \/-->/', $post_content, $matches)) {
+            foreach ($matches[1] as $pattern_id) {
+                $pattern_content = get_post($pattern_id)->post_content;
+                check_and_enqueue_child_block_admin_styles($pattern_content, $child_theme_blocks_path, $child_theme_blocks);
+            }
+        }
+    }
+}
+add_action('enqueue_block_editor_assets', 'dream_enqueue_child_block_admin_styles');
 
 //function dream_enqueue_child_block_admin_styles() {
 //    $child_theme_blocks_path = dirname(__DIR__, 2) . '/src/templates/blocks';
@@ -154,36 +232,3 @@ add_action('enqueue_block_assets', 'dream_enqueue_child_block_styles');
 //    }
 //}
 //add_action( 'enqueue_block_editor_assets', 'dream_enqueue_child_block_admin_styles' );
-
-function dream_enqueue_child_block_admin_styles() {
-    $child_theme_blocks_path = dirname(__DIR__, 2) . '/src/templates/blocks';
-    $child_theme_blocks = array_filter(scandir($child_theme_blocks_path), 'filter_block_dir');
-
-    // Get the post content
-    $post_content = get_post()->post_content;
-
-    foreach ($child_theme_blocks as $block) {
-        // Get the block.json file path
-        $block_json_path = $child_theme_blocks_path . '/' . $block . '/block.json';
-
-        if (file_exists($block_json_path)) {
-            // Read the block.json file
-            $block_json_content = file_get_contents($block_json_path);
-            $block_data = json_decode($block_json_content, true);
-
-            // Check if the block name is defined in block.json
-            if (isset($block_data['name'])) {
-                $block_name = $block_data['name'];
-
-                // Check if the post content contains the block
-                if (has_block($block_name, $post_content)) {
-                    // Enqueue the child theme block admin style
-                    if (file_exists($child_theme_blocks_path . '/' . $block . '/index.css')) {
-                        wp_enqueue_style('child_blocks_admin_style_' . $block, get_stylesheet_directory_uri() . '/src/templates/blocks/' . $block . '/index.css', array(), wp_get_theme()->get('Version'), 'all');
-                    }
-                }
-            }
-        }
-    }
-}
-add_action('enqueue_block_editor_assets', 'dream_enqueue_child_block_admin_styles');
