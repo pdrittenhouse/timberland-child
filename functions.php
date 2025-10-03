2e1c30b5c90355594d284acbce2ceb6c155f9e17
P.D. Rittenhouse <pdee999@gmail.com>
2025-10-03 01:20:04 -0400

    Update functions.php

--- functions.php
+++ functions.php
@@ -4,6 +4,7 @@
  * Include settings from /src/functions
  */
 $dream_child_includes = array(
+    "cache.php",
     "scripts.php",
     "styles.php",
     "blocks.php",
@@ -17,4 +18,5 @@ $dream_child_includes = array(
 foreach($dream_child_includes as $inc){
     include_once(get_stylesheet_directory() . "/src/functions/$inc");
 }
+
 
