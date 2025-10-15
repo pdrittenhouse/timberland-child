1e8acbe2be69cf93cc8b736d8676aa617ed2907a
Patrick Rittenhouse <pdee999@gmail.com>
2025-10-15 18:50:47 -0400

    adding viewscript

---
+++ src/templates/blocks/slide/view.js
@@ -0,0 +1,4 @@
+// Block: slide - View Script (Frontend Only)
+// This file is loaded only on the frontend, NOT in the editor
+// Use this for scripts that should only run on the public-facing site
+
