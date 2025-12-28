b017f1d87deb2d9d98fae290091b5dacd60cc6c9
Patrick Rittenhouse <pdee999@gmail.com>
2025-12-27 22:14:21 -0500

    adding alert to header

--- src/functions/widgets.php
+++ src/functions/widgets.php
@@ -1,9 +1,11 @@
 <?php
 
+
 // // Rename existing sidebars
 // function update_sidebar_names($sidebar)
 // {
 //     global $wp_registered_sidebars;
+
 
 //     if ('Primary' === $sidebar['name']) {
 //         $id = $sidebar['id'];
@@ -11,11 +13,13 @@
 //         $wp_registered_sidebars[$id] = $sidebar;
 //     }
 
+
 //     if ('Secondary' === $sidebar['name']) {
 //         $id = $sidebar['id'];
 //         $sidebar['name'] = 'New Secondary Name';
 //         $wp_registered_sidebars[$id] = $sidebar;
 //     }
+
 
 //     if ('Tertiary' === $sidebar['name']) {
 //         $id = $sidebar['id'];
@@ -23,13 +27,16 @@
 //         $wp_registered_sidebars[$id] = $sidebar;
 //     }
 
+
 //     return;
 // }
 // add_action('register_sidebar', 'update_sidebar_names');
 
+
 // // Register new sidebars
 // function child_widgets_init()
 // {
+
 //     register_sidebar(array(
 //         'name'          => 'Widget Area Name',
 //         'id'            => 'widget_area_name',
@@ -38,5 +45,6 @@
 //         'before_title'  => '<h2>',
 //         'after_title'   => '</h2>',
 //     ));
+
 // }
 // add_action('widgets_init', 'child_widgets_init');
